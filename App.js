59dbb25dbbbfe4fce08d976c0a0d66316b3a0863
justib41 <46506710+bethelju@users.noreply.github.com>
2020-06-16 19:16:00 -0700

    Adding scrolling, begin splitting items

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import TaskItem from './components/TaskItem'
 
 export default function App() {
+  const [enteredTask, setEnteredTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const taskInputHandler = (enteredText) => {
+    setEnteredTask(enteredText);
+  }
+
+  const addTaskHandler = () => {
+    setTasks(currentTasks => [...currentTasks, 
+      {key: Math.random().toString(), value: enteredTask}]);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput placeholder="Task"
-          style={styles.textInput} />
-        <Button title="Add" />
+          style={styles.textInput}
+          onChangeText={taskInputHandler}
+          value={enteredTask} />
+        <Button title="Add" onPress={addTaskHandler} />
       </View>
-      <View>
-      </View>
+      <FlatList 
+        data={tasks} 
+        renderItem={itemData => (
+          <TaskItem title={itemData.item.value} />
+        )
+      }/>   
     </View>
   );
 }
@@ -29,5 +48,5 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 5
-  },
+  } 
 });
